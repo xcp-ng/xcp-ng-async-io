84c32e37d320e87b5ba2de198ec5781a4693d1bf
Ronan Abhamon <ronan.abhamon@vates.fr>
2019-12-17 17:17:31 +0100

    feat(examples/copy-file): supports polling

--- examples/copy-file.c
+++ examples/copy-file.c
@@ -16,6 +16,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -127,21 +128,23 @@ static int copy (XcpIoQueue *queue, int in, int out, off_t inSize) {
       return ret;
 
     // 2. Write to out.
-    struct pollfd fds;
-    fds.events = POLLIN;
-    fds.fd = xcp_io_queue_get_event_fd(queue);
-    fds.revents = 0;
+    if (!xcp_io_queue_polling_enabled(queue)) {
+      struct pollfd fds;
+      fds.events = POLLIN;
+      fds.fd = xcp_io_queue_get_event_fd(queue);
+      fds.revents = 0;
 
-    do {
-      ret = poll(&fds, 1, 0);
-    } while (ret == -1 && errno == EINTR);
-    if (ret < 0)
-      return -errno;
-    if (ret) {
-      xcp_io_queue_process_responses(queue);
-      if ((ret = queue_submit(queue)) < 0)
-        return ret;
+      do {
+        ret = poll(&fds, 1, 0);
+      } while (ret == -1 && errno == EINTR);
+      if (!ret)
+        continue;
+      if (ret < 0)
+        return -errno;
     }
+
+    if ((ret = xcp_io_queue_process_responses(queue)) < 0)
+      return ret;
   }
 
   assert(xcp_io_queue_get_inflight_count(queue) == 0);
@@ -153,19 +156,68 @@ static int copy (XcpIoQueue *queue, int in, int out, off_t inSize) {
 
 // -----------------------------------------------------------------------------
 
+static void usage (const char *progname) {
+  printf("Usage: %s [OPTIONS]\n", progname);
+  puts("  --int                    input file");
+  puts("  --out                    output file");
+  puts("  --polling                use polling");
+  puts("  --help                   print this help and exit");
+}
+
+// -----------------------------------------------------------------------------
+
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    fprintf(stderr, "usage: %s <in> <out>\n", argv[0]);
+  const struct option longopts[] = {
+    { "in", 1, NULL, 'i' },
+    { "out", 1, NULL, 'o' },
+    { "polling", 0, NULL, 'p' },
+    { "help", 0, NULL, 'h' },
+    { NULL, 0, 0, 0 }
+  };
+
+  char *inPath = NULL;
+  char *outPath = NULL;
+
+  bool usePolling = false;
+
+  int option;
+  int longindex = 0;
+  while ((option = getopt_long_only(argc, argv, "", longopts, &longindex)) != -1) {
+    switch (option) {
+      case 'i':
+        inPath = optarg;
+        break;
+      case 'o':
+        outPath = optarg;
+        break;
+      case 'p':
+        usePolling = true;
+        break;
+      case 'h':
+        usage(argv[0]);
+        return EXIT_SUCCESS;
+      case '?':
+        if (optopt == 0)
+          fprintf(stderr, "Unknown option: `%s`.\n", argv[optind - 1]);
+        else
+          fprintf(stderr, "Error parsing option: `-%c`\n", optopt);
+        fprintf(stderr, "Try `%s --help` for more information.\n", *argv);
+        return EXIT_FAILURE;
+    }
+  }
+
+  if (!inPath || !outPath) {
+    fprintf(stderr, "in and/or out are not set!\n");
     return EXIT_FAILURE;
   }
 
-  const int in = open(argv[1], O_RDONLY);
+  const int in = open(inPath, O_RDONLY);
   if (in < 0) {
     perror("Failed to open input file");
     return EXIT_FAILURE;
   }
 
-  const int out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  const int out = open(outPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (out < 0) {
     perror("Failed to open output file");
     return EXIT_FAILURE;
@@ -180,7 +232,7 @@ int main(int argc, char *argv[]) {
   int ret;
 
   XcpIoQueue queue;
-  if ((ret = xcp_io_queue_init(&queue, QUEUE_CAPACITY, false)) < 0) {
+  if ((ret = xcp_io_queue_init(&queue, QUEUE_CAPACITY, usePolling)) < 0) {
     fprintf(stderr, "Failed to initialize queue: %s\n", strerror(-ret));
     return EXIT_FAILURE;
   }
